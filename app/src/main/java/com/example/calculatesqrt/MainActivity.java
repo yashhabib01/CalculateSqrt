dcda2cfaa5cb136d43eaec6608267db1154c9610
yashhabib01 <yash.d.habib1@gmail.com>
2021-08-13 21:21:58 -0500

    Ui Update

--- app/src/main/java/com/example/calculatesqrt/MainActivity.java
+++ app/src/main/java/com/example/calculatesqrt/MainActivity.java
@@ -7,38 +7,124 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
  public class MainActivity extends AppCompatActivity {
 
-    private EditText sqrt_num;
-    private Button sqrt_btn  ,clear_text;
+     Button button0,button1,button2,button3,button4,button5,button6,button7,button8
+             ,button9,buttonSqrt,button10, buttonC;
+
+     EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sqrt_btn = (Button) findViewById(R.id.sqrt_button);
-        clear_text = (Button)findViewById(R.id.text_clear);
-        sqrt_num = (EditText) findViewById(R.id.sqrt_num);
 
-        sqrt_btn.setOnClickListener(new View.OnClickListener() {
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        button10 = (Button) findViewById(R.id.button10);
+        buttonC = (Button) findViewById(R.id.buttonC);
+        editText  = (EditText) findViewById(R.id.edt1);
+        buttonSqrt = (Button) findViewById(R.id.buttoneql);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!TextUtils.isEmpty(sqrt_num.getText().toString())  &&
-                        sqrt_num.getText().toString().matches("\\d+(?:\\.\\d+)?")){
-                  double sqrt = Double.parseDouble(sqrt_num.getText().toString());
-                    sqrt_num.setText( "" + Math.sqrt(sqrt));
+                editText.setText(editText.getText() + "1");
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + "2");
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + "3");
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + "4");
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                editText.setText(editText.getText() + "5");
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + "6");
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + "7");
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + "8");
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + "9");
+            }
+        });
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + "0");
+            }
+        });
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
+            }
+        });
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + ".");
+            }
+        });
+
+        buttonSqrt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!TextUtils.isEmpty(editText.getText().toString()) && editText.getText().toString().matches("\\d+(?:\\.\\d+)?") ) {
+
+                    Double num  = Double.parseDouble(editText.getText().toString());
+                    num = Math.sqrt(num);
+
+                    editText.setText("" + num);
+
+                }else{
+                    Toast.makeText(MainActivity.this, "Invalid Input", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-        clear_text.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sqrt_num.setText("");
-            }
-        });
-
 
 
     }
